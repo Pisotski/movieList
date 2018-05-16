aea6f3bb264e5875b4b6b1d52d32f139914c8e6a
Poisk <vpisotski@gmail.com>
2018-05-15 21:54:19 -0700

    localhost is working. Environment set. got Hello World from App component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render() {
+    return(
+      <div className="App">
+        <h1> Movie List! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+window.App = App;
